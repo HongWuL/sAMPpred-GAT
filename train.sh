12d61305e3237c85b66310348f7981999cc1d1fb
HongWuL <73174239+HongWuL@users.noreply.github.com>
2022-09-23 15:37:10 +0800

    Add files via upload

--- train.sh
+++ train.sh
@@ -3,8 +3,8 @@
 pos=data/train_data/positive
 neg=data/train_data/negative
 
-# python generate_features.py -hhm_ifasta $pos/XU_pretrain_train_positive.fasta -hhm_oa3m $pos/a3m/ -hhm_ohhm $pos/hhm/ -hhm_tmp $pos/tmp/ -tr_ia3m $pos/a3m/ -tr_onpz $pos/npz/ -pssm_ifasta $pos/example_pos.fasta -pssm_opssm $pos/pssm/
-# python generate_features.py -hhm_ifasta $neg/XU_pretrain_train_negative.fasta -hhm_oa3m $neg/a3m/ -hhm_ohhm $neg/hhm/ -hhm_tmp $neg/tmp/ -tr_ia3m $neg/a3m/ -tr_onpz $neg/npz/ -pssm_ifasta $neg/example_neg.fasta -pssm_opssm $neg/pssm/
+python generate_features.py -hhm_ifasta $pos/XU_pretrain_train_positive.fasta -hhm_oa3m $pos/a3m/ -hhm_ohhm $pos/hhm/ -hhm_tmp $pos/tmp/ -tr_ia3m $pos/a3m/ -tr_onpz $pos/npz/ -pssm_ifasta $pos/example_pos.fasta -pssm_opssm $pos/pssm/
+python generate_features.py -hhm_ifasta $neg/XU_pretrain_train_negative.fasta -hhm_oa3m $neg/a3m/ -hhm_ohhm $neg/hhm/ -hhm_tmp $neg/tmp/ -tr_ia3m $neg/a3m/ -tr_onpz $neg/npz/ -pssm_ifasta $neg/example_neg.fasta -pssm_opssm $neg/pssm/
 
 python train.py -pos_t $pos/XU_pretrain_train_positive.fasta -pos_v $pos/XU_pretrain_val_positive.fasta -pos_npz $pos/npz/ \
                 -neg_t $neg/XU_pretrain_train_negative.fasta -neg_v $neg/XU_pretrain_val_negative.fasta -neg_npz $neg/npz/ \
